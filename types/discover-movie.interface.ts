fed74bd57cf9b78d6ab46e1e48945a274b28c5eb
oysterlee <oyster.lee@victoriahome.co>
2020-05-05 22:09:27 +0800

    Add discover movie interface

---
+++ types/discover-movie.interface.ts
@@ -0,0 +1,20 @@
+export interface IDiscoverMovie {
+  page: number;
+  total_results: number;
+  total_pages: number;
+  results: {
+    popularity: number;
+    id: number;
+    video: false;
+    vote_count: number;
+    vote_average: number;
+    title: string;
+    release_date: string;
+    original_language: string;
+    genre_ids: number[];
+    backdrop_path: string;
+    adult: boolean;
+    overview: string;
+    poster_path: string;
+  }[];
+}

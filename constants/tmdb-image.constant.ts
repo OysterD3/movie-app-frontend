96dde8168a73f7cecac026dc6b08ada33de5ed33
oysterlee <oyster.lee@victoriahome.co>
2020-05-05 22:26:28 +0800

    COnstant for image

---
+++ constants/tmdb-image.constant.ts
@@ -0,0 +1,24 @@
+export const BASE_URL = "http://image.tmdb.org/t/p";
+export const SECURE_BASE_URL = "https://image.tmdb.org/t/p";
+
+export const BACKDROP_SIZES = ["w300", "w780", "w1280", "original"];
+export const LOGO_SIZES = [
+  "w45",
+  "w92",
+  "w154",
+  "w185",
+  "w300",
+  "w500",
+  "original"
+];
+export const POSTER_SIZES = [
+  "w92",
+  "w154",
+  "w185",
+  "w342",
+  "w500",
+  "w780",
+  "original"
+];
+export const PROFILE_SIZES = ["w45", "w185", "h632", "original"];
+export const STILL_SIZES = ["w92", "w185", "w300", "original"];

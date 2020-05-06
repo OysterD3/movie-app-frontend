521f64e76aa88cbbcb001096a0d47cb921facf15
oysterlee <oyster.lee@victoriahome.co>
2020-05-06 21:50:37 +0800

    Add container styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app>
-    <v-content>
+    <v-content class="oys-container">
       <nuxt />
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "nuxt-property-decorator"
+import { Vue, Component } from "nuxt-property-decorator";
 
 @Component({
   name: "DefaultLayout"

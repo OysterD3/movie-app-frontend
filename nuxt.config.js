fef52a7a07c9d58e72d635078077651f6d3e5794
oysterlee <oyster.lee@victoriahome.co>
2020-05-06 22:57:02 +0800

    Install device package

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,14 @@ module.exports = {
     // Doc: https://axios.nuxtjs.org/usage
     "@nuxtjs/axios",
     // Doc: https://github.com/nuxt-community/dotenv-module
-    "@nuxtjs/dotenv"
+    "@nuxtjs/dotenv",
+    [
+      "@nuxtjs/device",
+      {
+        defaultUserAgent:
+          "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Mobile Safari/537.36"
+      }
+    ]
   ],
   /*
    ** Axios module configuration

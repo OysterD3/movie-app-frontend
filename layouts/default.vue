779fe400542104bee11860eff254272c0186b30e
oysterlee <oyster.lee@victoriahome.co>
2020-05-07 19:05:34 +0800

    Add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,15 @@
     <v-content class="oys-container">
       <nuxt />
     </v-content>
+    <v-divider class="mt-3" />
+    <v-footer padless class="transparent">
+      <v-col cols class="text-center">
+        Developed by
+        <a href="https://github.com/OysterD3" class="white--text">Oyster</a>
+        with
+        <a href="https://www.themoviedb.org/" class="white--text">TheMovieDB</a>
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
